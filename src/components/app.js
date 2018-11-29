ef9d7a35dcd8d4eefd0ae86a79d2cf913ac080fc
Tiffany Kim <minikim9@hotmail.com>
2018-11-29 13:40:56 -0800

    completed initial routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize';
+import About from './about';
+import Hidden from './hidden';
+import Home from './home';
+import Nav from './nav';
+import Public from './public';
+import signIn from './sign_in';
+import signUp from './sign_up';
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
+        <Nav/>
+        <div className="container">
+            <Route exact path = "/" component = {Home}/>
+            <Route path = "/about" component = {About}/>
+            <Route path = "/hidden" component = {Hidden}/>
+            <Route path = "/public" component = {Public}/>
+            <Route path = "/signIn" component = {signIn}/>
+            <Route path = "/signUp" component = {signUp}/>
         </div>
     </div>
 );
---
+++ src/components/nav.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom';
+
+class Nav extends Component{
+    render(){
+        return(
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to ="/" className="brand-logo">FISH COUNTS</Link>
+                    <ul className ="right">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/hidden">Hidden</Link>
+                        </li>
+                        <li>
+                            <Link to="/public">public</Link>
+                        </li>
+                        <li>
+                            <Link to="/signIn">sign in</Link>
+                        </li>
+                        <li>
+                            <Link to="/signUp">sign Up</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Nav;
